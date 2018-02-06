b80e227762c876ccc0cda20041602d3b17b3317c
Sumanth-istar <sumanth@istarindia.com>
2018-02-06 12:13:41 +0530

    create course task completed

--- src/app/create-course-task/create-course-task.component.ts
+++ src/app/create-course-task/create-course-task.component.ts
@@ -6,7 +6,14 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http/';
 import { Course } from '../pojo/course';
 import { Module } from '../pojo/module/module';
 import { INgxMyDpOptions, IMyDateModel } from 'ngx-mydatepicker';
-
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 @Component({
   selector: 'app-create-course-task',
   templateUrl: './create-course-task.component.html',
@@ -23,6 +30,11 @@ export class CreateCourseTaskComponent implements OnInit {
   newCourse: Course;
   progressWidth2 = 0;
   isInclude2ndStep = false;
+  users = [];
+  selectedUser = null;
+  @ViewChild('userSelectionInstance') userSelectionInstance: NgbTypeahead;
+  userSelectfocus$ = new Subject<string>();
+  userSelectclick$ = new Subject<string>();
   @ViewChild(WizardComponent)
   public wizard: WizardComponent;
 
@@ -54,6 +66,16 @@ export class CreateCourseTaskComponent implements OnInit {
     // date selected
   }
 
+
+  userSearch = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200).distinctUntilChanged()
+      .merge(this.userSelectfocus$)
+      .merge(this.userSelectclick$.filter(() => !this.userSelectionInstance.isPopupOpen()))
+      .map(term => (term === '' ? this.users : this.users.filter(v => v.email.toLowerCase().indexOf(term.toLowerCase()) > -1)));
+    userFormatter = (x: { email: string, id: number }) => x.id + ' ' + x.email;
+
+
   ngOnInit() {
 
     var modules = Array();
@@ -61,6 +83,17 @@ export class CreateCourseTaskComponent implements OnInit {
     const local_complex_object = localStorage.getItem('currentUser')
 
     this.complex_object = JSON.parse(local_complex_object);
+
+
+    this.http.get(AppConfiguration.ServerWithApiUrl + 'user/user_bytype/CONTENT_CREATOR').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.users = data['data'];
+      console.log(this.users);
+
+
+
+
+    });
   }
 
   isValidForm() {
@@ -105,6 +138,32 @@ export class CreateCourseTaskComponent implements OnInit {
       this.currentprogress = 0;
       this.isOn = true;
     }
+
+  }
+
+
+
+
+
+  finishFunction(){
+
+console.log(this.newCourse);
+console.log(this.selectedUser);
+console.log(this.model.formatted);
+
+
+var assignee_object = {
+  "userAssingedTo": [this.selectedUser.id],
+  "dueDate": this.model.formatted
+};
+const body = new HttpParams().set('course_object', JSON.stringify(this.newCourse)).set('assignee_object', JSON.stringify(assignee_object));
+this.http.post(AppConfiguration.ServerWithApiUrl + 'course/1/create_course_task/' + this.complex_object.id, body, {
+  headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
+}).subscribe(res => {
+  console.log(res['data']);
+});
+
+
 
   }
 
