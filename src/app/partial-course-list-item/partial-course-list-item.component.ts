71c01c2de3f9276ced4b78fb322dbae402832407
vinayistar <vinay@istarindia.com>
2018-04-18 19:26:45 +0530

    dropbox changes..implementation pending

--- src/app/partial-course-list-item/partial-course-list-item.component.ts
+++ src/app/partial-course-list-item/partial-course-list-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { AppConfiguration } from '../app.constants';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+declare var require: any;
+const swal = require('sweetalert2');
 @Component({
   selector: 'app-partial-course-list-item',
   templateUrl: './partial-course-list-item.component.html',
@@ -7,11 +10,27 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PartialCourseListItemComponent implements OnInit {
   @Input() data: any;
+  complex_object;
+  constructor(private http: HttpClient) {
 
-  constructor() { }
-
-  ngOnInit() {
-    //console.log(this.data);
   }
 
+  ngOnInit() {
+    const local_complex_object = localStorage.getItem('currentUser')
+
+    this.complex_object = JSON.parse(local_complex_object);
+    //console.log(this.data);
+  }
+  saveToDropbox(lesson) {
+    swal("Functionality pending");
+    /* const body = new HttpParams().set('course_object', JSON.stringify("{}"));
+    this.http.post(AppConfiguration.ServerWithApiUrl + 'lesson/1/upload_to_google/' + lesson.id + '/' + this.complex_object.id, body, {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
+    }).subscribe(res => {
+      //console.log(res['data']);
+      res['data'];
+    }, error => {
+      console.log('something went wrong');
+    }); */
+  }
 }
